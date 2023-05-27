a7d039e71c98061a5f26679efe13a15c858b6c2a
superdudu330 <guillaume.dupont330@outlook.fr>
2023-05-27 15:38:17 +0200

    second commit

--- _projet pro SDV/Scrapping pwsh/Scrapping_Pilot.ps1
+++ _projet pro SDV/Scrapping pwsh/Scrapping_Pilot.ps1
@@ -25,7 +25,7 @@ $csv_out_file = "$PSScriptRoot\export_csv\$($date)_cve.csv"
 ##Microsoft Security Update Guide RSS
 #'https://api.msrc.microsoft.com/update-guide/rss'
 $url_rss_feed = 'https://www.cvedetails.com/vulnerability-feed.php?vendor_id=0&product_id=0&version_id=0&orderby=1&cvssscoremin=0'
-$url_rss_feed = 'https://api.msrc.microsoft.com/update-guide/rss'
+
 
 #Exemple
 ##Microsoft Security Update Guide RSS
@@ -53,34 +53,36 @@ Write-Host -ForegroundColor Green "$items_number résultats trouvés sur le feed $
 # Afficher les liens dans la console powershell
 $Objects_in_feed.link
 
-# Décommentez la ligne suivante pour afficher le résultat dans une fenêtre native windows (Out-GridView)
-#$Objects_in_feed | Out-GridView
 
 #exportation des objects dans un CSV
 $Objects_in_feed | Export-Csv -Path $csv_out_file  -Delimiter ';' -NoTypeInformation
+
+# commentez la ligne suivante pour masquer le résultat dans une fenêtre native windows (Out-GridView)
+$Objects_in_feed | Out-GridView
 
 
 
 # /!\ Décommentez pour l'envoi du csv par mail /!\
 <#
-
 #$creds = Get-Credential       #Décommentez si authentification requise sur votre serveur SMTP
 
-$smtp_server = 'email-smtp.us-east-2.amazonaws.com'
-$from = 'nirvadrix@gmail.com'
-$to = 'nirvadrix@gmail.com'
+$smtp_server = ''    # mettre votre SMTP Server
+$from = ''           # adresse d'envoi
+$to = ''             # adresse de destination
+$Attachments = ''    # chemin du fichier à joindre au mail
+
+$object = "test"     # objet du mail
+$body = "test"       # corps du mail
 
 
-$object = "test"
-$body = "test"
-
-Send-MailMessage -From $from -To $to -SmtpServer $smtp_server -Subject $object -Body $body #-Credential $creds
-
+Send-MailMessage -From $from -To $to -SmtpServer $smtp_server -Subject $object -Body $body -Attachments $Attachments #-Credential $creds
 
 #>
 
+
     
 #End
+
 pause 
 
 
